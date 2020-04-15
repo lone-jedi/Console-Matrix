7ca22b9e0979bb413a3cba8facbd1f3f2c0c7d60
Alexander Yarkin <copy@yarkin.com.ua>
2020-04-15 21:55:46 +0300

    Fix bugs + add lighting 1. Fixed console window 2. Add left lighting animation 3. Loop sound

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -3,7 +3,6 @@
 #include <ctime>
 #include <string>
 #include "options.h"
-
 #pragma comment (lib, "winmm.lib")
 #pragma warning(disable : 4996) // PlaySound() - old function
 
@@ -11,14 +10,28 @@ using namespace std;
 
 int main()
 {
+	/* Fixed console window */
+	HWND hWnd;
+	HMENU hm;
+	if ((hWnd = GetConsoleWindow()) == NULL)
+	{
+		cout << "No console window!?\n";
+		return 0;
+	}
+	hm = GetSystemMenu(hWnd, FALSE);
+	RemoveMenu(hm, SC_SIZE, MF_BYCOMMAND | MF_REMOVE);
+	RemoveMenu(hm, SC_MAXIMIZE, MF_BYCOMMAND | MF_REMOVE);
+	DrawMenuBar(hWnd);
+
 	SetConsoleTextAttribute(hConsole, (WORD)((Black << 4) | Green));
-	system("mode con cols=100 lines=40"); /* Console window size. Width = STRING_SIZE symbols, height = ... lines */
 	SetConsoleTitleA("Matrix");
+
+	/* !!! Console window size. Width = STRING_SIZE symbols, height = ... lines */
+	system("mode con cols=100 lines=30"); 
 	srand(time(0));
-	int exit = false;
 
 	// Play main theme The Matrix, start title
-	if (PlaySound("wav/fullTheme.wav", NULL, SND_FILENAME | SND_ASYNC))
+	if (PlaySound("wav/fullTheme.wav", NULL, SND_LOOP | SND_FILENAME | SND_ASYNC))
 	{
 		speedPrint("PlaySound - init \n", 100);
 		speedPrint("Matrix ", 100);
@@ -27,17 +40,22 @@ int main()
 		speedPrint("...", 1000);
 		speedPrint("You are in matrix \n", 100);
 		speedPrint("Knock, knock User ", 100);
-		speedPrint("..\n", 1000);
+		speedPrint("..\n", 1000); 
 	}
 	else
 		cout << "PlaySound - ERROR " << strerror(GetLastError()) << endl;
 
-
 	// Print Matrix
-	while (!exit)
+	for(int j = 1; ; ++j)
 	{
 		for (int i = 0; i < STRING_SIZE; ++i)
 		{
+			if (j == LINE_SIZE)
+			{
+				lightningStruck(3 + rand() % 6);
+				j = 1;
+			}
+
 			if (i % WHITESPACE_PERIOD == 0 || rand() % 4 == 0)
 			{
 				cout << " ";
@@ -46,16 +64,16 @@ int main()
 
 			if (rand() % 10 == 0)
 			{
-				SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | White));
-				printSymb();
-				SetConsoleTextAttribute(hConsole, (WORD)((Black << 4) | Green));
+				printSymb(White);
 				continue;
 			}
+
 			printSymb();
 		}
 		Sleep(100);
 		cout << endl;
 	}
+
 	return 0;
 }
 
@@ -69,8 +87,43 @@ void speedPrint(string str, int speed)
 }
 
 
-void printSymb()
+void printSymb(ConsoleColor color, int codeAcsii)
 {
-	int value = FIRST_CODE_ASCII + rand() % (LAST_CODE_ASCII - FIRST_CODE_ASCII + 1);
-	cout << char(value);
+	int value;
+
+	if (codeAcsii == -1)
+		value = FIRST_CODE_ASCII + rand() % (LAST_CODE_ASCII - FIRST_CODE_ASCII + 1);
+	else
+		value = codeAcsii;
+
+	if (color != Green)
+	{
+		SetConsoleTextAttribute(hConsole, (WORD)((Black << 4) | color));
+		cout << char(value);
+		SetConsoleTextAttribute(hConsole, (WORD)((Black << 4) | Green));
+	}
+	else
+		cout << char(value);
+}
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(hConsole, coord);
+}
+
+void lightningStruck(int step, LightningSide side)
+{
+	short middle = LINE_SIZE / 2;
+	short randEndX = rand() % (STRING_SIZE / 3) + STRING_SIZE / 2;
+
+	for (short i = rand() % (STRING_SIZE / 6); i < randEndX; ++i)
+	{
+		SetConsoleCursorPosition(hConsole, { i , middle + rand() % (short)step - (short)step }); //XCoord = i, YCoord = [ -step ; +step]
+		cout << "\b";
+		printSymb(White);
+	}
+	SetConsoleCursorPosition(hConsole, { 0, 29 }); // Cursor to left bottom position 
 }
--- Matrix/options.h
+++ Matrix/options.h
@@ -2,6 +2,8 @@
 #include "windows.h"
 
 const int STRING_SIZE = 100;
+const int LINE_SIZE = 30;
+
 const int WHITESPACE_PERIOD = 10;	/* Input whitespace in string period*/
 int FIRST_CODE_ASCII = 179;
 int LAST_CODE_ASCII = 223;
@@ -24,5 +26,8 @@ enum ConsoleColor {
 	Yellow = 14,
 	White = 15
 };
-void printSymb();
+enum LightningSide {Left, Right};
+void printSymb(ConsoleColor color = Green, int codeAcsii = -1);
 void speedPrint(std::string str, int speed); /* slow motion text function */
+void gotoxy(int x, int y);
+void lightningStruck(int step, LightningSide side = Left);
